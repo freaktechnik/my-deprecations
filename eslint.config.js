1e4809d6e52cb6f3b1d43cfa7b9794403730ca4a
Martin Giger <martin@humanoids.be>
2024-05-11 14:57:48 +0200

    chore: upgrade to eslint v9

--- eslint.config.js
+++ eslint.config.js
@@ -1,16 +1,3 @@
 import freaktechnikConfigNode from "@freaktechnik/eslint-config-node";
 
-export default [
-    ...freaktechnikConfigNode,
-    {
-        files: [ "**/*.js" ],
-        rules: {
-            "unicorn/import-index": [
-                "warn",
-                {
-                    "ignoreImports": true,
-                },
-            ],
-        },
-    },
-];
+export default freaktechnikConfigNode;
